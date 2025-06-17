7ea2d8d533d80dad2bacdfa2cd41e2073f9dd010
Amirul Irfan <amirulirfan.mka@gmail.com>
2025-06-17 20:51:07 +0800

    changed

--- lab_6q1.php
+++ lab_6q1.php
@@ -1,18 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Lab 6 Q1</title>
+  <title>Lab 6 Q1</title>
 </head>
 <body>
-     <?php
-        $name = "Muhammad Asyraf";
-    ?>
-    <table>
-        <tr>
-            <td>Name</td>
-            <td><?php echo "$name"; ?></td>
-        </tr>
- </table>
-
+  <?php
+    $name = "Muhammad Asyraf";
+    $matric = "A123456";
+    $course = "Bachelor of Information Technology";
+    $year = "Year 2";
+    $address = "Parit Raja, Johor";
+  ?>
+  <table border="1" cellpadding="5">
+    <tr>
+      <td>Name</td>
+      <td><?php echo $name; ?></td>
+    </tr>
+    <tr>
+      <td>Matric Number</td>
+      <td><?php echo $matric; ?></td>
+    </tr>
+    <tr>
+      <td>Course</td>
+      <td><?php echo $course; ?></td>
+    </tr>
+    <tr>
+      <td>Year of Study</td>
+      <td><?php echo $year; ?></td>
+    </tr>
+    <tr>
+      <td>Address</td>
+      <td><?php echo $address; ?></td>
+    </tr>
+  </table>
 </body>
 </html>
